e27b257c3eb79a3755a6376e3c537c62a0d79653
deepanshu2711 <deepanshusaini2711@gmail.com>
2023-12-27 23:06:16 +0530

    header UI created

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
     return (
-        <div>
-            HEADER
+        <header className='bg-custom_green-100 shadow-md '>
+        <div className='flex justify-between p-3 max-w-6xl mx-auto items-center'>
+        <h1 className='font-bold text-3xl'><span className='text-custom_green-200'>Real</span> <span className='text-custom_green-400'>Estate</span></h1>
+            <form className='flex items-center bg-white p-3 rounded-lg'>
+                <input className=' bg-transparent focus:outline-none' type='text' placeholder='search' />
+                <FaSearch className='text-custom_green-300' />
+            </form>
+        <ul className='flex gap-4 font-medium text-custom_green-400'>
+            <Link to='/'>
+            <li className=' hover:underline cursor-pointer'>Home</li>
+            </Link>
+            <Link to='/about'>
+            <li className=' hover:underline cursor-pointer'>About</li>
+            </Link>
+            <Link to='/signin'>
+            <li className=' hover:underline cursor-pointer'>Sign In</li>
+            </Link>
+        </ul>
         </div>
+            
+        </header>
     );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "custom_green":{
+          100 : "#D2E3C8",
+          200:"#86A789",
+          300 :"#739072",
+          400:"#4F6F52"
+        }
+      }
+    },
   },
   plugins: [],
 }
