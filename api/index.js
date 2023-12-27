ff2a57106b9e9be829bf03caec4e0a33b67cd3af
deepanshu2711 <deepanshusaini2711@gmail.com>
2023-12-27 23:34:21 +0530

    setup mongo DB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 
+mongoose.connect(process.env.MONGO).then(()=>console.log("DB connected"))   //application string
 
 const app = express();
 
 app.listen(3000,()=>{
     console.log("Server Running on port 3000..")
-})
+});
