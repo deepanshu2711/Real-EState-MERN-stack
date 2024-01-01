6e886f66b9d1aded4145d5238649c076cf13fc56
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-01-01 13:16:41 +0530

    cpmplte header search functionality

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,16 +1,33 @@
 import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import{useSelector} from "react-redux"
+import { useEffect, useState } from 'react';
 
 function Header(props) {
+    const[searchTerm,setsearchTerm] = useState("");
+    const navigate = useNavigate();
     const {currentUser} = useSelector((state) => state.user);
+    function handleSubmit(e) {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);   ///Builtin  javascript constructor;
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+    useEffect(() =>{
+        const urlParams = new URLSearchParams(window.location.search);
+        const searchTerm = urlParams.get('searchTerm');
+        if(searchTerm) {
+            setsearchTerm(searchTerm);
+        }
+    },[location.search]);
     return (
         <header className='bg-custom_green-100 shadow-md '>
         <div className='flex justify-between p-3 max-w-6xl mx-auto items-center'>
         <h1 className='font-bold text-3xl'><span className='text-custom_green-200'>Real</span> <span className='text-custom_green-400'>Estate</span></h1>
-            <form className='flex items-center bg-white p-3 rounded-lg'>
-                <input className=' bg-transparent focus:outline-none' type='text' placeholder='search' />
-                <FaSearch className='text-custom_green-300' />
+            <form className='flex items-center bg-white p-3 rounded-lg' onSubmit={handleSubmit}>
+                <input id='search' value={searchTerm} onChange={(e) => setsearchTerm(e.target.value)} className=' bg-transparent focus:outline-none' type='text' placeholder='search' />
+                <button><FaSearch className='text-custom_green-300' /></button>
             </form>
         <ul className='flex gap-4 font-medium text-custom_green-400'>
             <Link to='/'>
